c0c6f0e27a603a59bf5144a38e7d07910c58cabb
Hiroyuki Sano <sh19910711@gmail.com>
2015-01-12 19:19:15 +0900

    runner: change constructor's args

--- src/runner.rb
+++ src/runner.rb
@@ -1,7 +1,7 @@
 class Solution::Runner
 
-  def self.run(input_format, &block)
-    Solution::Solver.new(input_format) do
+  def self.run(input_format, input_stream = STDIN, output_stream = STDOUT, &block)
+    Solution::Solver.new(input_format, input_stream, output_stream) do
       input do |args|
         if block.nil?
           output solve(*args)
