7b6726d397c25210722a9a76f8fa24e6e1bf3cd4
Hiroyuki Sano <sh19910711@gmail.com>
2015-01-12 19:19:03 +0900

    solver: change constructor args

--- src/solver.rb
+++ src/solver.rb
@@ -1,9 +1,14 @@
 class Solution::Solver
 
   attr_reader :input_format
+  attr_reader :input_stream
+  attr_reader :output_stream
 
-  def initialize(new_input_format, &block)
+  def initialize(new_input_format, new_input_stream, new_output_stream, &block)
     @input_format = new_input_format
+    @input_stream = new_input_stream
+    @output_stream = new_output_stream
+
     instance_eval(&block) if block_given?
   end
 
